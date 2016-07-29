35af52cc908e2201a7226ae75c304d21e044c55f
GOWREESWARI23 <gowrisubramanianfire@gmail.com>
2016-07-29 11:21:02 +0530

    Create rah.java

---
+++ rah.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class rah
+{
+int a[]={6,2,3,9,1);
+int s=1;
+for(int i=1;i<a.length;i++)
+    {
+    for (int j =0;j<a.length;j++)
+    {
+    if (a[i]<s)
+    {
+    s=a[i];
+    if (a[j]<seconds)
+    {
+     seconds=a[j];
+    }
+    }
+    }
+    }
+    }
